7c06ced1a018756b9200f5aa5372c51a3be4da76
eduvs68 <eduvs68@gmail.com>
2024-10-08 17:06:00 -0300

    background consertado, header consertada, troquei o gradiente de cor da header para o certo, adicionei o serviço httpClient na app.config.ts para começar a usar a api.

---
+++ src/app/show-results/show-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-results.component.html',
+  styleUrl: './show-results.component.css'
+})
+export class ShowResultsComponent {
+
+}
