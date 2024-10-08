bec3cec80c6b316d4782a117b1b9267a7e4629ac
Miguel Santos <miguellsantoss915@gmail.com>
2024-10-07 22:09:21 -0300

    avanços no projeto

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+
+}
